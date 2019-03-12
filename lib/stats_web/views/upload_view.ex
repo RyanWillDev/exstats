42a0a2f5a8c566fa6c0dc21c5a9c997a7cbbd873
Ryan Will <ryanwilldev@gmail.com>
2019-03-12 13:04:57 -0500

    adds view for upload responses

---
+++ lib/stats_web/views/upload_view.ex
@@ -0,0 +1,14 @@
+defmodule StatsWeb.UploadView do
+  use StatsWeb, :view
+
+  def render("duplicate_entries.json", _) do
+    %{
+      details: %{
+        error: "Duplicate values were uploaded"
+      }
+    }
+  end
+
+  def render("error.json", %{message: message}) do
+  end
+end
